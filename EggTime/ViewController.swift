a726b94ad5f25d7cd067c86eba6fef91dfe62a90
marufkhan09 <maruf1609@gmail.com>
2023-09-19 01:49:47 +0600

    alarm sound added

--- EggTime/ViewController.swift
+++ EggTime/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     let softTime = 5
@@ -17,6 +18,8 @@ class ViewController: UIViewController {
     var secondPassed = 0
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var timerlabel: UILabel!
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         timerlabel.text = ""
         super.viewDidLoad()
@@ -102,12 +105,19 @@ class ViewController: UIViewController {
             
             } else {
                 self.timerlabel.text = "Done"
+                self.playSound(input: "alarm_sound")
                 self.secondPassed = 0
                 self.progressBar.setProgress(0.0, animated: true)
                 Timer.invalidate()
             }
         }
         
+    }
+    func playSound(input: String) {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
     }
     
 }
